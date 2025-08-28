358d93b8da51cfe880d387f3aba8db5c9a1d8063
FiTcHeRs71 <frederic.ducrot@hotmail.com>
2025-08-28 17:12:58 +0200

    Add files via upload

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -12,11 +12,48 @@
 
 #include <unistd.h>
 
+
 int ft_atoi(char *str)
 {
-	
+	int	i;
+	int	v;
+	int	ngps;
+
+	i = 0;
+	v = 0;
+	ngps = 1;;
+
+		while((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		{
+			i++;
+		}
+		while(str[i] == '+' || str[i] == '-')
+		{
+			if(str[i] == '-')
+			{
+				ngps *= -1;
+			}
+			i++;
+		}
+		while(str[i] <= '9' && str[i] >= '0')
+		{
+			v = (v * 10) + (str[i] - '0');
+			i++;
+		}
+		v *= ngps;
+		return (v);
 }
-int	main(int argv, char **argc)
+/*int	main()
 {
+	char	*a = "     ---+--+-1234ac56)";
+	int	retour;
 	
-}
+	ft_atoi(a);
+	retour = ft_atoi(a);
+
+
+
+
+	return (0);
+
+}*/
